c4c953c2eb012c0674c7f936516d68a6a0b33bdc
Ayush_d172005 <ayushd172005@gmail.com>
2026-06-10 09:23:08 +0530

    Create SOL10.py

---
+++ SOL10.py
@@ -0,0 +1,5 @@
+Developed and optimized machine learning models for predictive analytics and data-driven decision-making.
+Performed data preprocessing, feature engineering, and exploratory data analysis on large datasets.
+Implemented and evaluated ML algorithms using Python libraries such as Scikit-learn, Pandas, NumPy, and TensorFlow/PyTorch.
+Collaborated with cross-functional teams to deploy AI solutions and improve model performance through continuous experimentation.
+Participated in model validation, performance monitoring, and documentation of machine learning workflows.
