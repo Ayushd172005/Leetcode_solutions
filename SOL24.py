a7e0a9c0965ed5d00163316990aec5f68c98b2dc
Ayush_d172005 <ayushd172005@gmail.com>
2026-07-02 20:09:59 +0530

    Create SOL24.py

---
+++ SOL24.py
@@ -0,0 +1,4 @@
+[02/07/26, 7:46:58 PM] Ad: yoo this i16 good?
+[02/07/26, 7:52:20 PM] Ad: spt mai 18 launch hora hai
+[02/07/26, 7:52:31 PM] Ad: so what is the best strat to use?
+[02/07/26, 7:53:08 PM] Ad: like should you wait for 2 months and get 17 or get 16 now or directly get 18 when launched?
